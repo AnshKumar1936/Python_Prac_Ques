b73c55522e01c85d4680449b7db7fd4e8e4a53d1
Ansh Kumar Singh <148905627+AnshKumar1936@users.noreply.github.com>
2024-07-05 14:05:11 +0530

    Add files via upload

---
+++ Ques15.py
@@ -0,0 +1,8 @@
+square = lambda x : x*x
+cube = lambda y : y*y*y
+
+x = int(input("Enter the value of X to calculate square:"))
+y = int(input("Enter the value of Y to calculate cube:"))
+
+print("The square of ",x, " is",square(x))
+print("The cube of ",y, " is",cube(y))
